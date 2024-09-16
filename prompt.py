e0ae5cd1a72d908e316e478980c3f654f32f9060
Federico Gonzalez <fgonzalez@kiusys.com>
2024-09-16 17:17:50 -0300

    el schema de la db es mas chico para mejor performance en pruebas

--- prompt.py
+++ prompt.py
@@ -19,24 +19,13 @@ Generate a SQL query that answers the question `{question}`.
 This query will run on a database whose schema is represented in this string:
 
 CREATE TABLE IF NOT EXISTS "csa_projects" (
-"client_name" TEXT,
-  "client_id" TEXT,
-  "PROJECT_ID" TEXT,
   "ebs_code" INTEGER, -- This is also known as Project for business purposes.
-  "project_name" TEXT,
-  "opportunity_name" TEXT,
-  "project_type" TEXT,
-  "project_type_abbreviation" TEXT,
   "SVP" TEXT, -- Senior Vice President
   "NBD" REAL,
-  "OPERATIONS_MANAGER" REAL,
+  "project_type" TEXT,
   "project_start_date" TEXT,
   "project_end_date" TEXT,
-  "project_status" REAL,
-  "business_unit" TEXT,
-  "opportunity_stage_name" TEXT,
-  "active" INTEGER, -- It has a 1 when active, and a 0 if inactive.
-  "closing_date" TEXT
+  "active" INTEGER -- It has a 1 when active, and a 0 if inactive.
 );
 
 CREATE TABLE IF NOT EXISTS "kpi" (
@@ -47,31 +36,8 @@ CREATE TABLE IF NOT EXISTS "kpi" (
   "KPI_Start_Date" TEXT,
   "KPI_End_Date" TEXT,
   "Progress_to_Goal_to_date" TEXT,
-  "Access" TEXT,
-  "CRM" TEXT,
-  "Client_Data_To_Syneos" TEXT,
-  "Reporting_Frequency" TEXT,
-  "Department_Head" REAL,
   "Reasons" REAL,
-  "Data_Date" TEXT, -- Date of the data. It´s always the last day of the month in format YYYY-MM-DD.
-  "Reporting_period" TEXT,
-  "Client_Services" TEXT,
-  "Project_Category" TEXT
-);
-
-CREATE TABLE IF NOT EXISTS "risk_share" (
-"Data_Date" TEXT,
-  "EBS_Project_ID" TEXT,
-  "Business_Line" TEXT,
-  "Actual_Pool" REAL,
-  "Earned" REAL,
-  "Act_Pct" REAL,
-  "Year" INTEGER,
-  "Timing" REAL,
-  "quarter" TEXT,
-  "Month_Recognized" TIMESTAMP,
-  "Sales_Ops" TEXT,
-  "Metric" TEXT
+  "Data_Date" TEXT -- Date of the data. It´s always the last day of the month in format YYYY-MM-DD.
 );
 
 CREATE TABLE IF NOT EXISTS "operational_risk" (
@@ -94,30 +60,6 @@ CREATE TABLE IF NOT EXISTS "revenues" (
 "BU" INTEGER, -- This is also known as Project for business purposes.
   "data_date" TEXT, -- Date of the data. It´s always the last day of the month in format YYYY-MM-DD.
   "revenue" REAL
-);
-
-CREATE TABLE IF NOT EXISTS "ic" (
-"data_date" TEXT, -- Date of the data. It´s always the last day of the month in format YYYY-MM-DD.
-  "F_Code" INTEGER,
-  "Business_Unit" TEXT,
-  "Period" TEXT,
-  "year" REAL,
-  "Plan_Status" TEXT,
-  "Amendments_Yr" REAL,
-  "Amendments_for_Target" REAL,
-  "Plan_Engagement_75" TEXT,
-  "Target_Earning" REAL,
-  "Actual_Earning" REAL,
-  "Actual_Payout" REAL,
-  "Earning_to_Originals" REAL,
-  "Earning_to_Original_Reason" TEXT,
-  "Comments" REAL
-);
-
-CREATE TABLE IF NOT EXISTS "trafficlight" (
-"BU" INTEGER,
-  "data_date" TEXT,
-  "colour" TEXT
 );
 
 CREATE TABLE IF NOT EXISTS "turnover" (
